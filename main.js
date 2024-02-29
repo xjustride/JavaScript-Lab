05a3b7a6b76491d487f4290490e5b064b58b0ddd
Remigiusz Krupa <115359807+xjustride@users.noreply.github.com>
2024-02-29 11:27:21 +0100

    finish

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const input1 = document.querySelector('#val1')
 const input2 = document.querySelector('#val2')
 const input3 = document.querySelector('#val3')
 const input4 = document.querySelector('#val4')
+const inputs = document.querySelector(`#inputs`)
 // console.dir(input1)
 // console.log(input1.value)
 
@@ -15,16 +16,17 @@ const avgval = document.querySelector(`#avg`)
 
 // reagowanie na klikniecie
 const przeliczBtn = document.querySelector(`#przelicz`)
-przeliczBtn.addEventListener(`click`, ()=>{
-    console.log(`User wdusil buttona`)
+inputs.addEventListener(`change`, ()=>{
     const min = Math.min(input1.value, input2.value, input3.value, input4.value)
     const max = Math.max(input1.value, input2.value, input3.value, input4.value)
-    const sum = input1.value + input2.value + input3.value + input4.value
+    const sum = parseInt(input1.value) + parseInt(input2.value) + parseInt(input3.value) + parseInt(input4.value)
     
       
     minval.textContent = `Wartość minimalna to: ` + min
     maxval.textContent = `Wartość maksymalna to: ` + max
     sumval.textContent = `Suma wartości to: ` + sum
-    avgval.textContent = `Średnia wartość to: ` + tot
+    avgval.textContent = `Średnia wartość to: ` + sum / 4
+
+    
 
 })
