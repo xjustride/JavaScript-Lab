5e22ba40051eec2a5c2411d4020209a8cf348849
Remigiusz <remigiusz.krupa@microsoft.wsei.edu.pl>
2024-03-13 23:32:01 +0100

    final version of slider

--- Lab2/main.js
+++ Lab2/main.js
@@ -1,3 +1,47 @@
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll("img");
+let aktualny = 0;
+const slajdy = document.querySelectorAll('.slajd');
+const kropki = document.querySelectorAll('.kropka');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
 
+const lenSlaid = slajdy.length;
+
+function aktualizujSlajd(indeks) {
+    
+  aktualny = (indeks + lenSlaid) % lenSlaid;
+  const x = -aktualny * 100;
+  document.querySelector('.box').style.transform = `translateX(${x}%)`;
+  zaktualizujKropki();
+  
+}
+
+function zaktualizujKropki() {
+  kropki.forEach((kropka, indeks) => {
+    if (indeks === aktualny) {
+      kropka.classList.add('aktywna');
+    } else {
+      kropka.classList.remove('aktywna');
+    }
+  });
+}
+
+  
+next.addEventListener('click', () => {
+    aktualizujSlajd(aktualny+1);
+  });
+
+  prev.addEventListener('click', () => {
+    aktualizujSlajd(aktualny-1);
+  });
+  
+  kropki.forEach((kropka, indeks) => {
+    kropka.addEventListener('click', () => {
+      aktualizujSlajd(indeks);
+    });
+  });
+
+setInterval(() => {
+  aktualizujSlajd(aktualny+1);
+}, 3000);
+
+zaktualizujKropki();
